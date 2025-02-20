96e9ba08f300cd82996012c1da86315c7a3afa2d
Thiago Cardoso <thiagocardoso0806@gmail.com>
2025-02-20 17:17:16 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,64 @@
-from assets.npcs import *
+from assets.events import *
 
-def attack_mob(mob):
-    mob["hp"] -= player["atk"]
+class control:
+    def __init__(self):
+        pass    
+    
+    def edit_inventory(self):
+        plin = inventory()
+        plin.show_inventory()
+        print(f"\033[32mCommands: add, del, exit, use, offhand\033[0m")
+        key = input("inventory> ")
+        if key == "exit":
+            pass
+        elif key == "del":
+            i = input("inventory_del> ")
+            plin.del_item(i)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"The item '{i}' was removed")
+            self.edit_inventory()
+        elif key == "add":
+            t = list(input("inventory_add> "))
+            h = int(t[0])
+            i = int(t[1])
+            #plin.add_item(h, i)
+            self.edit_inventory()
+        elif key == "use":
+            i = input("inventory_use> ")
+            plin.use_item(i)
+            self.edit_inventory()
+        elif key == "offhand":
+            player().off_hand()
+        elif key == "ggg":
+            l = input("")
+            plin.search(l)
+        else:
+            print("\033[31mCommand not founded\033[0m")
+            self.edit_inventory()
 
-def name_gender():
-    print("Select your attributes, please.")
-    player["name"] = input("Name: ")
-    player["gender"] = input("Gender [m/f]: ")
+    def terminal(self):
+        key = input("> ")
+        if key == "inventory":
+            self.edit_inventory()
+            self.terminal()
+        elif key == "stats":
+            player().show_player()
+            self.terminal()
+        elif key == "help":
+            print(f"\033[32mAvaliable commands: exit, help, inventory, stats\033[0m")
+            self.terminal()
+        elif key == "exit":
+            print("Thanks for playing")
+            pass
+        elif key == "cls":
+            os.system('cls' if os.name == 'nt' else 'clear')
+            self.terminal()
+        elif key == "battle":
+            Mobspawner().spawn_mob(1)
+            battle(0).loop()
+            self.terminal()
+        else:
+            print("\033[31mCommand not founded, please type help for commands\033[0m")
+            self.terminal()
 
-spawn_mob(6)
-show_mobs()
-
-select_mob = monsters[0]
-
-#print("Selected Mob: ", select_mob)
-#attack_mob(select_mob)
-if select_mob["hp"] == 0 or select_mob["hp"] <= 0:
-        monsters.remove(select_mob)
-        print("Mob Killed")
-else:
-    print("Attacked Mob: ", select_mob)
-
+control().terminal()
