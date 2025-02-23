3f619bdaf9446729e1634d3c830b4b70a69540a0
Thiago Cardoso <thiagocardoso0806@gmail.com>
2025-02-23 14:59:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,38 +7,34 @@ class control:
     def edit_inventory(self):
         plin = inventory()
         plin.show_inventory()
-        print(f"\033[32mCommands: add, del, exit, use, offhand\033[0m")
+        print(f"\033[32mCommands: add, drop, exit, use, offhand, offartc\033[0m")
         key = input("inventory> ")
         if key == "exit":
             pass
-        elif key == "del":
-            i = input("inventory_del> ")
-            plin.del_item(i)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(f"The item '{i}' was removed")
+        elif key == "drop":
+            plin.del_item()
             self.edit_inventory()
         elif key == "add":
-            t = list(input("inventory_add> "))
+            t = list(input("inv_add> "))
             h = int(t[0])
             i = int(t[1])
-            #plin.add_item(h, i)
+            plin.add_item(h, i)
+            os.system('cls' if os.name == 'nt' else 'clear')
             self.edit_inventory()
         elif key == "use":
-            i = input("inventory_use> ")
-            plin.use_item(i)
+            plin.use_item()
             self.edit_inventory()
         elif key == "offhand":
             player().off_hand()
-        elif key == "ggg":
-            l = input("")
-            plin.search(l)
+        elif key == "offartc":
+            player().amulet()
         else:
             print("\033[31mCommand not founded\033[0m")
             self.edit_inventory()
 
     def terminal(self):
         key = input("> ")
-        if key == "inventory":
+        if key == "inv":
             self.edit_inventory()
             self.terminal()
         elif key == "stats":
@@ -55,10 +51,11 @@ class control:
             self.terminal()
         elif key == "battle":
             Mobspawner().spawn_mob(1)
-            battle(0).loop()
+            battle(0, False).loop()
             self.terminal()
         else:
             print("\033[31mCommand not founded, please type help for commands\033[0m")
             self.terminal()
 
+Tower().Start()
 control().terminal()
